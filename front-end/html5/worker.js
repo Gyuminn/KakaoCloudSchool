f60ddf28f633af6b566fa5040bb65e8f5bec646e
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-11-15 16:35:57 +0900

    (feat: JS HTML5) practice web worker

---
+++ front-end/html5/worker.js
@@ -0,0 +1,10 @@
+// html에서 요청이 왔을 때
+onmessage = (event) => {
+  // html에서 전송한 데이터 받기
+  let num = event.data;
+  let result = 0;
+  for (let i = 1; i <= num; i++) {
+    result += i;
+  }
+  postMessage(result);
+};
