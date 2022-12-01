cdc09dfb249498bf07f0974cabff8f72c0b48ced
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-12-01 15:43:54 +0900

    (feat: Nodejs) add Domain model, domains database

---
+++ back-end/node-api-server/models/domain.js
@@ -0,0 +1,36 @@
+const Sequelize = require("sequelize");
+module.exports = class Domain extends Sequelize.Model {
+  // 테이블에 대한 설정
+  static init(sequelize) {
+    return super.init(
+      {
+        host: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+        },
+        clientSecret: {
+          type: Sequelize.STRING(36),
+          allowNull: false,
+        },
+        type: {
+          type: Sequelize.ENUM("free", "premium"),
+          allowNull: false,
+        },
+      },
+      {
+        // 테이블에 대한 설정
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "Domain",
+        tableName: "domains",
+        paranoid: true,
+      }
+    );
+  }
+
+  // 관계에 대한 설정
+  static associate(db) {
+    db.Hashtag.belongsToMany(db.Post, { through: "PostHashtag" });
+  }
+};
