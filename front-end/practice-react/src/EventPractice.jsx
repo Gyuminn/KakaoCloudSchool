b401516bf738847840b6db09c260cc63a93d0067
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-12-05 15:46:56 +0900

    (feat: React) practice Event Routing & onKeyPress

--- front-end/practice-react/src/EventPractice.jsx
+++ front-end/practice-react/src/EventPractice.jsx
@@ -7,22 +7,30 @@ class EventPractice extends Component {
   //     this.handleClick = this.handleClick.bind(this);
   //   }s
   state = {
-    name: "gyumin",
+    name: "",
+    message: "",
   };
 
   // babel이 인스턴스의 메서드로 변환을 자동으로 수행
   // this.handleChange로 이 베서드를 사용하는 것이 가능.
   handleChange = (e) => {
     this.setState({
-      name: e.target.value,
+      [e.target.name]: e.target.value,
     });
   };
 
   handleClick = (e) => {
-    alert(this.state.name);
+    alert(this.state.name + ":" + this.state.message);
     this.setState({
       name: "",
+      message: "",
     });
+  };
+
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.handleClick();
+    }
   };
   render() {
     return (
@@ -30,10 +38,19 @@ class EventPractice extends Component {
         <h1>useState를 이용한 상태 관리</h1>
         <input
           type="text"
-          name="message"
+          name="name"
           placeholder="이름을 입력하세요"
           value={this.state.name}
           onChange={this.handleChange}
+        />
+
+        <input
+          type="text"
+          name="message"
+          placeholder="메시지을 입력하세요"
+          value={this.state.message}
+          onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
         />
         <button onClick={this.handleClick}>학인</button>
       </>
