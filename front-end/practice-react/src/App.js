557188f5ddeedb1caa156c202db1520a4fd04421
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-12-05 12:18:55 +0900

    (feat: React) practice useState at class component

--- front-end/practice-react/src/App.js
+++ front-end/practice-react/src/App.js
@@ -1,4 +1,5 @@
 import MyComponent from "./MyComponent";
+import StateComponent from "./StateComponent";
 
 export default function App() {
   return (
@@ -6,6 +7,7 @@ export default function App() {
       <MyComponent name="gyumin" year={97}>
         어쩌구 저쩌구
       </MyComponent>
+      <StateComponent />
     </>
   );
 }
