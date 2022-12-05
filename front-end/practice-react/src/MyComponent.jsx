d6b1a5145b5767d1917f486c66c742b26f08e5a2
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-12-05 11:23:19 +0900

    (feat: React) convert class component for props

--- front-end/practice-react/src/MyComponent.jsx
+++ front-end/practice-react/src/MyComponent.jsx
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 
 import PropTypes from "prop-types";
+class MyComponent extends Component {
+  render() {
+    // props 가져오기
+    const { name, year, children } = this.props;
+    return (
+      <>
+        <div>안녕하세요 제 이름은 {name} 입니다.</div>
+        <p>children 값은 {children}</p>
+        <p>제가 태어난 해는 {year} 입니다.</p>
+      </>
+    );
+  }
+}
 
-const MyComponent = ({ name, year, children }) => {
-  return (
-    <>
-      <div>나의 이름은 {name} 이다.</div>
-      <p>나이는 {year}에 태어났다.</p>
-      <p>{children}</p>
-    </>
-  );
+MyComponent.defaultProps = {
+  name: "기본이름",
 };
 
 MyComponent.propTypes = {
   name: PropTypes.string,
   year: PropTypes.number.isRequired,
-};
-
-MyComponent.defaultProps = {
-  name: "gyumin",
 };
 
 export default MyComponent;
