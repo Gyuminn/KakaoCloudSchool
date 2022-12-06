de699e80cc56d8882eaac686e64feded48664019
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-12-06 17:49:57 +0900

    (etc: Nodejs) test server

--- front-end/practice-react/src/App.js
+++ front-end/practice-react/src/App.js
@@ -1,27 +1,30 @@
+import React, { Component } from "react";
 import EventPractice from "./EventPractice";
 import MyComponent from "./MyComponent";
 import ScrollBox from "./ScrollBox";
 import StateComponent from "./StateComponent";
 import ValidationSample from "./ValidationSample";
 
-export default function App() {
-  return (
-    <>
-      <MyComponent name="gyumin" year={97}>
-        어쩌구 저쩌구
-      </MyComponent>
-      <StateComponent />
-      <EventPractice />
-      <br />
-      <ValidationSample />
-      <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-      <button
-        onClick={(e) => {
-          this.scrollBox.scrollToBottom();
-        }}
-      >
-        맨 아래로
-      </button>
-    </>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <MyComponent name="gyumin" year={97}>
+          어쩌구 저쩌구
+        </MyComponent>
+        <StateComponent />
+        <EventPractice />
+        <br />
+        <ValidationSample />
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button
+          onClick={(e) => {
+            this.scrollBox.scrollToBottom();
+          }}
+        >
+          맨 아래로
+        </button>
+      </>
+    );
+  }
 }
