dd9f5e458a5804e41ad60566ffaaa441586df0a9
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-12-07 12:44:24 +0900

    (feat: React) practice scss,  mixin & import

--- front-end/react-styling/src/App.js
+++ front-end/react-styling/src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
 import CSSModule from "./CSSModule";
 
+import styles from "./App.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
 function App() {
+  const isBlue = true;
+
   return (
     <>
+      <div className={cx("box", { blue: isBlue })}>
+        <div className={cx("box-inside")} />
+      </div>
       <CSSModule />
     </>
   );
