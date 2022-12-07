a3d7bb5fdb357cb7fb39fad1a8cc543bccbfdee0
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-12-07 14:47:31 +0900

    (feat: React) practice responsive web by include-media

--- front-end/react-styling/src/App.js
+++ front-end/react-styling/src/App.js
@@ -3,6 +3,7 @@ import CSSModule from "./CSSModule";
 
 import styles from "./App.scss";
 import classNames from "classnames/bind";
+import Button from "./components/Button";
 
 const cx = classNames.bind(styles);
 
@@ -15,6 +16,7 @@ function App() {
         <div className={cx("box-inside")} />
       </div>
       <CSSModule />
+      <Button>버튼</Button>
     </>
   );
 }
