f404d42cb4139453fdafb2e21e71fb8297281cd2
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-12-07 15:35:19 +0900

    (feat: React) add material design link & files

--- front-end/react-styling/src/App.js
+++ front-end/react-styling/src/App.js
@@ -1,22 +1,28 @@
 import "./App.css";
-import CSSModule from "./CSSModule";
+// import CSSModule from "./CSSModule";
 
-import styles from "./App.scss";
-import classNames from "classnames/bind";
-import Button from "./components/Button";
+// import styles from "./App.scss";
+// import classNames from "classnames/bind";
+// import Button from "./components/Button";
 
-const cx = classNames.bind(styles);
+// const cx = classNames.bind(styles);
 
 function App() {
-  const isBlue = true;
+  // const isBlue = true;
 
   return (
     <>
-      <div className={cx("box", { blue: isBlue })}>
+      {/* <div className={cx("box", { blue: isBlue })}>
         <div className={cx("box-inside")} />
       </div>
       <CSSModule />
-      <Button>버튼</Button>
+      <Button>버튼</Button> */}
+      <nav>
+        <div className="nav-wrapper">
+          <div>리액트</div>
+        </div>
+      </nav>
+      <div>머터리얼 디자인</div>
     </>
   );
 }
