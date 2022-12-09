aaa2699b4f2fd87dd8f246262003a79c7cbbce11
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-12-09 17:44:00 +0900

    (feat: React) install react-redux, practice manage global state by redux

--- front-end/react-redux/src/index.js
+++ front-end/react-redux/src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import exercise from "./exercise";
+// import "./exercise";
+
+import rootReducer from "./modules";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
+// console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
