ac5cd70c6f11843772cf4dec9c13bdc60d01c1e5
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-12-15 16:27:01 +0900

    (feat: Java) practice getter, setter, recursion

---
+++ back-end-java/javabasic/src/kakao/itstudy/oop/Main.java
@@ -0,0 +1,81 @@
+package kakao.itstudy.oop;
+
+import java.lang.reflect.Method;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        // Student 클래스의 인스턴스 생성
+        Student student = new Student();
+        // 자신의 속성에 접근
+        student.num = 1;
+        student.name = "김규민";
+        student.kor = 32;
+        student.eng = 19;
+        student.mat = 40;
+
+        Student student1 = new Student();
+        student1.num = 2;
+
+        System.out.println(student.num);
+        System.out.println(student1.num);
+
+        // 자바는 static 속성에 인스턴스가 접근 가능
+        student.schoolName = "강릉고등학교";
+        student1.schoolName = "한양대학교";
+        // static 속성은 모든 인스턴스가 공유하므로
+        // 동일한 데이터가 출력된다.
+        System.out.println(student.schoolName);
+        System.out.println(student1.schoolName);
+     */
+        // static 메서드는 클래스 이름으로 호출 가능
+        MethodClass.method1();
+        // 클래스 이름을 가지고 메서드 호출 불가
+        // MethodClass.method2();
+
+        // 인스턴스 메서드 호출
+        MethodClass methodClass = new MethodClass();
+        methodClass.method2();
+        // 자바는 인스턴스를 이용해서 static 메서드 호출 가능
+        methodClass.method1();
+
+        // 참조형 변수를 만들지 않고 인스턴스를 만들어서 메서드 호출
+        // 딱 한번만 사용할 인스턴스는 이름을 만들지 않는 것이 좋다.
+        // 연결된 변수가 없기 때문에 언제든지 인스턴스 메모리 해제를 할 수 있다.
+        new MethodClass().method2();
+
+        MethodClass.noArgsMethod();
+        MethodClass.oneArgsMethod(3);
+        MethodClass.twoArgsMethod(2, "Message");
+
+        MethodClass obj = new MethodClass();
+        int result = obj.addWithInteger(10,30);
+        System.out.println(result);
+        result = obj.addWithInteger(result, 90);
+        System.out.println(result);
+
+        int x = 10;
+        MethodClass.callByValue(x);
+        // 기본형을 메서드에게 넘겨준 경우는 데이터가 변경되지 않음.
+        System.out.println("x:" + x);
+
+        int [] br = {10, 20, 30};
+        MethodClass.callByReference(br);
+        // 배열을 메서드에게 넘기면 배열의 내용이 변경될 수도 있음.
+        // 메서드의 리턴이 없는 경우라면 print 메서드를 제외하고는
+        // 원본을 변경해주는 것이다.
+        System.out.println("br[0]:" + br[0]);
+
+        MethodClass o1 = new MethodClass();
+        o1.thisMethod();
+
+        // 20번째 피보나치 수열의 값
+        int f = MethodClass.noRecursionFibonacci(50);
+        System.out.println("f:" + f);
+
+        int f2 = MethodClass.recursionFibonacci(50);
+        System.out.println("f2:" + f2);
+    }
+}
+
---
+++ back-end-java/javabasic/src/kakao/itstudy/oop/MethodClass.java
@@ -0,0 +1,102 @@
+package kakao.itstudy.oop;
+
+public class MethodClass {
+    private static int num;
+    private String name;
+
+    // static method
+    public static void method1() {
+        num = 1;
+        // static 메서드에서는 instance 속성이 안됨.
+        // name = "gyumin";
+        System.out.println("static method");
+    }
+
+    // instance method
+    public void method2() {
+        num = 1;
+        name = "gyumin";
+        System.out.println("instance method");
+    }
+
+    // static: 인스턴스를 생성할 필요가 없음.
+    // 매개 변수가 없음.
+    // MethodClass.noArgsMethod() 로 호출한다.
+    public static void noArgsMethod() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("method");
+        }
+    }
+
+    // 매개변수가 정수 1개인 메서드
+    // MethodClass.oneArgsMethod(정수) 로 호출한다.
+    public static void oneArgsMethod(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("method");
+        }
+    }
+
+    // 매개변수가 2개인 메서드
+    // 매개변수가 2개 이상인 경우는 순서대로 대입을 해야 한다.
+    public static void twoArgsMethod(int n, String msg) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(msg);
+        }
+    }
+
+    // 리턴이 없는 메서드는 연속해서 호출하는 것이 불가능
+    // 2개의 정수를 받아서 더해누는 메서드
+    // 인스턴스.addWithInteger(정수1, 정수2)로 호출
+    public int addWithInteger(int first, int second) {
+        return (first + second);
+    }
+
+    // 매개 변수가 기본형 1개
+    // 내부에서 매개변수의 값을 수정해도 넘겨준 데이터는 변경되지 않음.
+    public static void callByValue(int n) {
+        n = n + 1;
+        System.out.println("n:" + n);
+    }
+
+    // 매개변수가 참조형인 경우
+    public static void callByReference(int[] ar) {
+        ar[0] = ar[0] + 1;
+        System.out.println("ar[0]:" + ar[0]);
+    }
+
+    public int score = 100;
+
+    public void thisMethod() {
+        int score = 200;
+        // scope의 법칙 적용 - 가까이에서 만든 것을 사용
+        System.out.println("score:" + score); // 200
+        // 인스턴스가 사진 score 호출
+        // this가 붙으면 메서드 안에서는 찾지 않
+        System.out.println("this.score:" + this.score);
+    }
+
+    public static int noRecursionFibonacci(int n) {
+        int n_1 = 1; // 현재 구하고자 하는 피보나치 값의 바로 앞
+        int n_2 = 1; // 현재 구하고자 하는 피보나치 값의 두 번째 앞
+        // 기본 값 설정
+        int fibo = 1;
+        // 규칙이 3번쨰 부터 적용
+        int i = 3;
+        while (i <= n) {
+            fibo = n_1 + n_2;
+            // n_1과 n_2를 이동
+            n_2 = n_1;
+            n_1 = fibo;
+            i++;
+        }
+        return fibo;
+    }
+
+    // 재귀를 이용한 피보나치 수열
+    public static int recursionFibonacci(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return recursionFibonacci(n - 1) + recursionFibonacci(n - 2);
+    }
+}
