4611e16bcff3488c6f2c695df8a494ed4366677a
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-12-16 10:22:25 +0900

    (feat: Java) initialize using constructor

---
+++ back-end-java/javabasic/src/kakao/itstudy/oop/ConstructorMain.java
@@ -0,0 +1,25 @@
+package kakao.itstudy.oop;
+
+import java.util.Date;
+
+public class ConstructorMain {
+    public static void main(String[] args) {
+        String[] nicknames = {"qmin", "Gyuminn"};
+        /*
+        // 인스턴스 생성
+        Member member = new Member();
+        member.setEmail("gyumin@gmail.com");
+        member.setPassword("1234");
+        member.setMarried(false);
+        member.setAge(26);
+        // 오늘 날짜
+        // member.setBirthday(new Date());
+        member.setBirthday(new Date(97, 0, 29));
+        member.setNicknames(nicknames);
+        System.out.println(member);
+        */
+
+        Member member1 = new Member("gyumin@gmail.com", "1234", nicknames, new Date(97, 0, 29), false, 26);
+        System.out.println(member1);
+    }
+}
---
+++ back-end-java/javabasic/src/kakao/itstudy/oop/Member.java
@@ -0,0 +1,97 @@
+package kakao.itstudy.oop;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class Member {
+    private String email;
+    private String password;
+    private String[] nicknames;
+    private Date birthday;
+    private boolean married;
+    private int age;
+
+    public Member(String email, String password, String[] nicknames, Date birthday, boolean married, int age) {
+        this.email = email;
+        this.password = password;
+        this.nicknames = nicknames;
+        this.birthday = birthday;
+        this.married = married;
+        this.age = age;
+    }
+
+    // 접근자 메서드
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String[] getNicknames() {
+        return nicknames;
+    }
+
+    public void setNicknames(String[] nicknames) {
+        this.nicknames = nicknames;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // 속성에 배열이나 List와 같은 컬렉션이 있을 때는 컬렉션 내에서
+    // 하나의 데이터만 읽고 쓸 수 있는 메서드를 제공해주는게 좋다.
+    public String getNickname(int index) {
+        return nicknames[index];
+    }
+
+    public void setNickname(int index, String nickname) {
+        nicknames[index] = nickname;
+    }
+
+    // 모든 속성의 toString을 호출해서 하나의 문자열로 만들어주는 메서드
+    // 출력하는 메서드에서 인스턴스 이름을 대입하면 자동 호출
+    // 디버깅(값을 확인하는 작업)을 위해서 호출하는 메서드
+    @Override
+    public String toString() {
+        return "Member{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", nicknames=" + Arrays.toString(nicknames) +
+                ", birthday=" + birthday +
+                ", married=" + married +
+                ", age=" + age +
+                '}';
+    }
+}
