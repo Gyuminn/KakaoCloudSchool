1a6dc3dc29dd5bb29f8a63008cbd10a6c3b1a896
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-12-20 15:36:26 +0900

    (feat: Java) practice Generic method and instance

--- back-end-java/javabasic/src/kakao/itstudy/java/lang/Main.java
+++ back-end-java/javabasic/src/kakao/itstudy/java/lang/Main.java
@@ -146,5 +146,14 @@ public class Main {
         }
         long endTime = System.currentTimeMillis();
         System.out.println((endTime - startTime) + "밀리초");
+
+        // Generics가 적용된 클래스의 인스턴스를 만들 때는
+        // 실제 자료형을 결정지어야 경고가 발생하지 않는다.
+        Generic<String> obj1 = new Generic<>("규민", "김규민", "kimkyumin", "gimgyumin", "Gyuminn");
+
+        obj1.disp();
+        // 기본형은 Generics에 적용할 수 없다.
+        Generic<Integer> obj2 = new Generic<>(100,200,30);
+        obj2.disp();
     }
 }
