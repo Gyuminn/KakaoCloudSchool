0554b13ca4b8879a6a95f14cb4bc9b5d23df33f8
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-12-21 15:20:03 +0900

    (feat: Java) practice Stack, Queue, Set & add feature of lotto generator

--- back-end-java/javabasic/src/kakao/itstudy/java/util/Main.java
+++ back-end-java/javabasic/src/kakao/itstudy/java/util/Main.java
@@ -71,18 +71,21 @@ public class Main {
         long start;
         long end;
         start = System.currentTimeMillis();
-        for(int i = 0; i<100000; i++) {
+        for (int i = 0; i < 100000; i++) {
             Integer integer = al.get(i);
         }
         end = System.currentTimeMillis();
         System.out.println("ArrayList 조회 시간: " + (end - start) + "ms");
 
+        /*
         start = System.currentTimeMillis();
         for(int i = 0; i<100000; i++) {
             Integer integer = li.get(i);
         }
         end = System.currentTimeMillis();
         System.out.println("LinkedList 조회 시간: " + (end - start) + "ms");
+
+         */
 
         List<Integer> al2 = new ArrayList<>();
         List<Integer> li2 = new LinkedList<>();
@@ -94,20 +97,20 @@ public class Main {
         li2.add(3);
 
         start = System.currentTimeMillis();
-        for(int i = 0; i<100000; i++) {
-            al2.add(1,2);
+        for (int i = 0; i < 100000; i++) {
+            al2.add(1, 2);
         }
         end = System.currentTimeMillis();
         System.out.println("ArrayList 중간 삽입 시간: " + (end - start) + "ms");
 
         start = System.currentTimeMillis();
-        for(int i = 0; i<100000; i++) {
-            li2.add(1,2);
+        for (int i = 0; i < 100000; i++) {
+            li2.add(1, 2);
         }
         end = System.currentTimeMillis();
         System.out.println("LinkedList 중간 삽입 시간: " + (end - start) + "ms");
 
-        List <String> list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         list.add("천규민");
         list.add("김규민");
         list.add("박규민");
@@ -115,7 +118,7 @@ public class Main {
         list.add("이규민");
 
         // 순회
-        for(String temp:list) {
+        for (String temp : list) {
             System.out.println(temp);
         }
 
@@ -127,5 +130,96 @@ public class Main {
             }
         });
         System.out.println(list);
+
+        Stack<String> stack = new Stack<>();
+        // 스택에 데이터 추가
+        stack.push("김규민");
+        stack.push("이규민");
+        stack.push("박규민");
+        System.out.println(stack);
+        System.out.println(stack.pop());
+
+        // 데이터를 sort된 것처럼 저장.
+        Queue<String> queue = new PriorityQueue<>();
+        queue.offer("박규민");
+        queue.offer("천규민");
+        queue.offer("이규민");
+        queue.offer("김규민");
+        System.out.println(queue);
+        System.out.println(queue.poll());
+
+        // 저장 순서를 알 수 없는 Set 인스턴스 생성
+        Set<String> set = new HashSet<>();
+        // 데이터 추가
+        set.add("천규민");
+        set.add("박규민");
+        set.add("김규민");
+        set.add("이규민");
+        // 동일한 데이터를 삽입 - equals 메서드로 비교해서
+        // true이면 삽입하지 않는다.
+        // Set은 데이터 삽입에 실패하면 false를 리턴하고
+        // 성공하면 true를 리턴한다.
+        set.add("김규민");
+        set.add("나규민");
+        System.out.println(set);
+
+
+        /**
+         * [] 로또 번호 생성기 만들기
+         * [] 1-45까지의 숫자 6개를 입력받아서 저장한 후 출력
+         * [] 입력을 받을 때 1 ~ 45 사이의 숫자가 아니면 다시 입력하도록
+         * [] 중복되는 숫자를 입력하면 다시 입력하도록 한다.
+         * [] 데이터를 출력할 때는 정렬을 해서 출력
+         */
+
+        // 숫자 6개를 저장할 공간을 생성
+        // 중복 검사가 수월하고 정렬을 수행하면서 삽입하는 TreeSet을 이용
+
+        // 배열을 이용하는 경우
+        // 입력받기 위한 인스턴스 생성
+        Scanner sc = new Scanner(System.in);
+        // 6개의 정수를 저장할 배열을 생성
+        int[] lotto = new int[6];
+
+        int len = lotto.length;
+        for (int i = 0; i < len; i++) {
+            try {
+                System.out.print("로토 번호 입력: ");
+                lotto[i] = sc.nextInt();
+
+                // 1부터 45사이의 숫자만 입력받도록 하기
+                if (lotto[i] < 1 || lotto[i] > 45) {
+                    System.out.println("1-45 사이의 숫자만 입력하세요!!");
+                    i--;
+                }
+
+                // 데이터 중복 검사
+                // 첫 번째부터 현재 데이터 앞까지 비교
+                boolean flag = false;
+                for (int j = 0; j < i; j++) {
+                    // 2개의 데이터가 같음
+                    if (lotto[i] == lotto[j]) {
+                        flag = true;
+                        break;
+                    }
+                }
+
+                // 중복된 경우
+                if(flag == true) {
+                    System.out.println("중복된 숫자 입니다.!!!");
+                    i--;
+                }
+            } catch (Exception e) {
+                // 예외의 경우 i를 다시 무효화 시켜서 총 입력 횟수를 유지해준다.
+                i--;
+                sc.nextLine();
+                System.out.println("숫자를 입력하세요!!");
+            }
+
+        }
+        // 배열의 데이터를 출력
+        System.out.println(Arrays.toString(lotto));
+        // 스캐너 정리
+        sc.close();
     }
 }
