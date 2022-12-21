786e31c171ca25a6f112c6a4296bb3754b2f8b6a
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-12-21 10:00:45 +0900

    (feat: Java) practice sort by 2nd parameter of Array.sort using Comparator<T>

--- back-end-java/javabasic/src/kakao/itstudy/java/util/Main.java
+++ back-end-java/javabasic/src/kakao/itstudy/java/util/Main.java
@@ -1,6 +1,7 @@
 package kakao.itstudy.java.util;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,7 +24,7 @@ public class Main {
 
         // VO 클래스의 인스턴스 5개를 소유하는 배열
         VO[] datas = new VO[5];
-        datas[0] = new VO(1,"나희도", 30);
+        datas[0] = new VO(1, "나희도", 30);
         datas[1] = new VO(2, "김규민", 27);
         datas[2] = new VO(3, "람머스", 28);
         datas[3] = new VO(4, "마동석", 20);
@@ -33,5 +34,21 @@ public class Main {
         // 에러
         Arrays.sort(datas);
         System.out.println(Arrays.toString(datas));
+
+        String[] names = {"리치히", "하일스베르", "로섬", "고슬링 "};
+        System.out.println(Arrays.toString(names));
+
+        //복사본 만들기
+        String[] copy = Arrays.copyOf(names, names.length);
+        Arrays.sort(copy);
+        System.out.println(Arrays.toString(copy));
+
+        Arrays.sort(copy, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        System.out.println(Arrays.toString(copy));
     }
 }
