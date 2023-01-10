578fd643ebe2458d27eb8d1ddb0513d30cc95b30
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2023-01-10 15:18:23 +0900

    (feat: SpringBoot) practice Page & Sorting

--- springbootjpa0110/src/main/java/com/kakao/springbootjpa0110/persistence/MemoRepository.java
+++ springbootjpa0110/src/main/java/com/kakao/springbootjpa0110/persistence/MemoRepository.java
@@ -3,5 +3,12 @@ package com.kakao.springbootjpa0110.persistence;
 import com.kakao.springbootjpa0110.domain.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+    // mno의 값이 from부터 to 사이인 데이터 조회하는 메서드
+    List<Memo> findByMnoBetween(Long from, Long to);
+
+    // mno의 값이 from부터 to 사이인 데이터를 Mno의 내림차순 정려해서 조회하는 메서드
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
 }
--- springbootjpa0110/src/test/java/com/kakao/springbootjpa0110/RepositoryTest.java
+++ springbootjpa0110/src/test/java/com/kakao/springbootjpa0110/RepositoryTest.java
@@ -5,7 +5,13 @@ import com.kakao.springbootjpa0110.persistence.MemoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -23,5 +29,95 @@ public class RepositoryTest {
                     .build();
             memoRepository.save(memo);
         });
+    }
+
+    @Test
+    public void testAll() {
+        List<Memo> list = memoRepository.findAll();
+        for(Memo memo: list) {
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void getById() {
+        // 기본키를 가지고 조회하면 없거나 1개의 데이터 리턴
+        // 그럴 때는 Optional을 사용해야 한다.
+        Optional<Memo> result = memoRepository.findById(1000L);
+
+        if(result.isPresent()) {
+            System.out.println(result.get());
+        } else {
+            System.out.println("데이터가 존재하지 않습니다.");
+        }
+    }
+
+    @Test
+    public void updateTest() {
+        // 기본 값이 존재하면 수정이지만 존재하지 않으면 삽입이다.
+        Memo memo = Memo.builder()
+                .mno(1000L)
+                .memoText("데이터 수정")
+                .build();
+        memoRepository.save(memo);
+    }
+
+    @Test
+    public void deleteTest() {
+        // 기본키를 가지고 삭제
+        memoRepository.deleteById(100L);
+        // Entity를 이용해서 삭제
+        memoRepository.delete(Memo.builder().mno(99L).build());
+
+        // 없는 데이터를 삭제하고자 하면 에러
+        // 삭제를 할 때는 존재 여부를 확인해야 한다.
+        memoRepository.deleteById(1000L);
+    }
+
+    @Test
+    public void testPaging() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        // 전체 페이지 개수
+        System.out.println(result.getTotalPages());
+
+        // 조회된 데이터 순회
+        for(Memo memo: result.getContent()) {
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testSort() {
+        // mno의 내림차순
+        Sort sort = Sort.by("mno").descending();
+        Pageable pageable = PageRequest.of(0, 10, sort);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        for(Memo memo : result.getContent()){
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testSortConcate() {
+        Sort sort1 = Sort.by("mno").descending();
+        Sort sort2 = Sort.by("memoText").ascending();
+        // 2개 결합 - sort1의 값이 같으면 sort2로 정렬
+        Sort sortAll = sort1.and(sort2);
+
+        Pageable pageable = PageRequest.of(0, 10, sortAll);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        for(Memo memo: result.getContent()) {
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void queryMethod1() {
+        List<Memo> list = memoRepository.findByMnoBetweenOrderByMnoDesc(30L, 40L);
+
+        for(Memo memo: list) {
+            System.out.println(memo);
+        }
     }
 }
