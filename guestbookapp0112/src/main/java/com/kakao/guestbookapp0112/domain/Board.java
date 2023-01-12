4ab807c672b37d441a6aa5313d8d47760f9285f2
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2023-01-12 09:45:28 +0900

    (feat: Spring Boot) 필요한 테이블에 대한 Entity 생성

---
+++ guestbookapp0112/src/main/java/com/kakao/guestbookapp0112/domain/Board.java
@@ -0,0 +1,23 @@
+package com.kakao.guestbookapp0112.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Board extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long bno;
+
+    private String title;
+
+    private String content;
+}
