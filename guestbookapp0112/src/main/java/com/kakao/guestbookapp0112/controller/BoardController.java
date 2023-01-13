f48d557c5c10aa6bce3920330aa72b723c8d2010
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2023-01-13 09:49:15 +0900

    (feat: Spring Boot) 게시물 목록보기, 삽입 뷰 구현

--- guestbookapp0112/src/main/java/com/kakao/guestbookapp0112/controller/BoardController.java
+++ guestbookapp0112/src/main/java/com/kakao/guestbookapp0112/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.kakao.guestbookapp0112.controller;
 
+import com.kakao.guestbookapp0112.dto.BoardDTO;
 import com.kakao.guestbookapp0112.dto.PageRequestDTO;
 import com.kakao.guestbookapp0112.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @Log4j2
@@ -20,5 +23,24 @@ public class BoardController {
         log.info("기본 목록 보기 요청");
         model.addAttribute("result", boardService.getList(pageRequestDTO));
         return "board/list";
+    }
+
+    // 게시물 등록 화면으로 이동하는 요청 - Fowarding
+    @GetMapping("/board/register")
+    public void register(Model model) {
+        log.info("등록 화면으로 포워딩");
+    }
+
+    // 게시물을 등록하는 요청 - Redirect
+    // RedirectAttributes - 1회용 ㅔ션
+    @PostMapping("/board/register")
+    public String register(BoardDTO dto, RedirectAttributes rattr) {
+        // 파라미터 확인
+        log.info("dto: " + dto.toString());
+        // 데이터 삽입
+        Long bno = boardService.register(dto);
+        rattr.addFlashAttribute("msg", bno + "등록");
+
+        return "redirect:/board/list";
     }
 }
