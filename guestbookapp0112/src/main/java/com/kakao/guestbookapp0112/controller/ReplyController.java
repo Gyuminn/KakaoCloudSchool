295b0d10c29f8d7d0d61fb3e9c84770426605f7a
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2023-01-16 10:56:48 +0900

    (feat: Spring Boot) 댓글 요청을 처리하는 RestController 추가 및 html 출력

--- guestbookapp0112/src/main/java/com/kakao/guestbookapp0112/controller/ReplyController.java
+++ guestbookapp0112/src/main/java/com/kakao/guestbookapp0112/controller/ReplyController.java
@@ -1,13 +1,30 @@
 package com.kakao.guestbookapp0112.controller;
 
+import com.kakao.guestbookapp0112.dto.ReplyDTO;
 import com.kakao.guestbookapp0112.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 @Log4j2
 @RequiredArgsConstructor
+@RequestMapping("/replies") // 공통 URL 설정
 public class ReplyController {
     private final ReplyService replyService;
+
+    // 게시글 번호를 가지고 댓글을 리턴해주는 메서드
+    @GetMapping(value="/board/{bno}")
+    public ResponseEntity<List<ReplyDTO>> getByBoard(@PathVariable("bno") Long bno) {
+        log.info("bno: " + bno);
+        // JSON 으로 주기
+        return new ResponseEntity<>(replyService.getList(bno), HttpStatus.OK);
+    }
 }
