4c3c5b7495bb6f03cf81e24402481f0a425914b4
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2023-01-16 14:02:27 +0900

    (feat: Spring Boot) 댓글 삭제을 위한 메서드 추가 및 jQuery 모달 구현

--- guestbookapp0112/src/main/java/com/kakao/guestbookapp0112/controller/ReplyController.java
+++ guestbookapp0112/src/main/java/com/kakao/guestbookapp0112/controller/ReplyController.java
@@ -4,6 +4,7 @@ import com.kakao.guestbookapp0112.dto.ReplyDTO;
 import com.kakao.guestbookapp0112.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,7 @@ public class ReplyController {
     private final ReplyService replyService;
 
     // 게시글 번호를 가지고 댓글을 리턴해주는 메서드
-    @GetMapping(value="/board/{bno}")
+    @GetMapping(value = "/board/{bno}")
     public ResponseEntity<List<ReplyDTO>> getByBoard(@PathVariable("bno") Long bno) {
         log.info("bno: " + bno);
         // JSON 으로 주기
@@ -31,5 +32,13 @@ public class ReplyController {
         log.info(replyDTO);
         Long rno = replyService.register(replyDTO);
         return new ResponseEntity<>(rno, HttpStatus.OK);
+    }
+
+    // 댓글 삭제 요청 처리
+    @DeleteMapping("/{rno}")
+    public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+        log.info("Rno:" + rno);
+        replyService.remove(rno);
+        return new ResponseEntity<>(rno + " 삭제", HttpStatus.OK);
     }
 }
