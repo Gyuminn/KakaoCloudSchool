920275ee75ef28e290409d75f2449a17fef9b86b
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2023-01-17 14:36:30 +0900

    (feat: Spring Boot) 이미지 미리보기 구현

--- reviewapp0116/src/main/java/com/kakao/reviewapp0116/controller/UploadController.java
+++ reviewapp0116/src/main/java/com/kakao/reviewapp0116/controller/UploadController.java
@@ -3,14 +3,19 @@ package com.kakao.reviewapp0116.controller;
 import com.kakao.reviewapp0116.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -44,9 +49,9 @@ public class UploadController {
         // 결과를 전달할 객체 생성
         List<UploadResultDTO> resultDTOList = new ArrayList<>();
 
-        for(MultipartFile uploadFile: uploadFiles) {
+        for (MultipartFile uploadFile : uploadFiles) {
             // 이미지 파일이 아니면 이미지 업로드 수행하지 않음.
-            if(uploadFile.getContentType().startsWith("image") == false) {
+            if (uploadFile.getContentType().startsWith("image") == false) {
                 log.warn("이미지 파일이 아님");
                 return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
@@ -71,11 +76,30 @@ public class UploadController {
                 uploadFile.transferTo(savePath);
                 // 결과를 List에 추가
                 resultDTOList.add(new UploadResultDTO(fileName, uuid, realUploadPath));
-            } catch(IOException e) {
+            } catch (IOException e) {
                 System.out.println(e.getLocalizedMessage());
                 e.printStackTrace();
             }
         }
         return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
+    }
+
+    @GetMapping("/display")
+    // 파일의 내용을 결과로 전송 - 바이트 배열을 타입으로 사용
+    public ResponseEntity<byte[]> getFile(String filename) {
+        ResponseEntity<byte []> result = null;
+        try {
+            log.warn("파일 이름:" + filename);
+            // 전송할 파일 생성 - 디코딩 필요
+            File file = new File(uploadPath + File.separator + URLDecoder.decode(filename, "UTF-8"));
+            // 헤더에 파일이라는 것을 설정
+            HttpHeaders header = new HttpHeaders();
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            // 파일의 내용을 응답의 결과로 생성
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
     }
 }
